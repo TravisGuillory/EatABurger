ccf5e4e551eec8a36fca53f9b0a0e7b7e6af8d9f
TravisGuillory <travisguillory70@gmail.com>
2020-04-23 01:31:32 -0500

    added heroku

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,24 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 
 if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB.URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '*',
-        database: 'burgers_db'
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "*",
+        database: "burgers_db"
     });
 };
 
 // -- Connect to the data base and handle connection error
 connection.connect((err) =>{
     if(err){
-        console.error('Error connecting: ' + err.stack);
+        console.error("Error connecting: " + err.stack);
     }
-    console.log('Connected as id ' + connection.threadId);
+    console.log("Connected as id " + connection.threadId);
 });
 
 module.exports = connection;
